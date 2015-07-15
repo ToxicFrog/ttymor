a95e10dce44d1fa20173ea538a3b24b4f80d3d03
Ben Kelly <bk@ancilla.ca>
2015-07-15 14:20:04 -0400

    Get the map renderer working again and add some performance statistics

--- components/map.lua
+++ components/map.lua
@@ -30,37 +30,57 @@ function map:try_move(ent, x, y)
   return self[x][y][1].name == "floor"
 end
 
-function map:render_screen(ent, view, cx, cy)
-  -- Map scrolling happens here. It's clamped to between -0 (near edge of map at
-  -- edge of screen) and -(map size - view size) (far edge of map at far edge
-  -- of screen).
-  -- This keeps cx,cy as close to the center of the screen as possible, without
-  -- allowing black space at the edge of the screen.
-  view.dx = -math.bound(0, cx - view.w/2, self.w - view.w):floor()
-  view.dy = -math.bound(0, cy - view.h/2, self.h - view.h):floor()
+function map:render_screen(ent, cx, cy)
+  local sw,sh = tty.size() -- screen width and height
+  local rw,rh -- render width and height
+  local ox,oy -- origin of render region
+  local dx,dy -- offset on screen
 
-  for x=1,self.w do
-    for y=1,self.h do
-      local _x,_y = x + view.dx, y + view.dy
-      if view.x < _x and _x <= view.x + view.w
-        and view.y < _y and _y <= view.y + view.h
-      then
-        local cell = self[x][y]
-        if #cell > 0 then
-          tty.put(x + view.dx, y + view.dy, cell[#cell]:render())
-        end
+  -- Map scrolling happens here.
+  -- If the map is smaller than the screen, center it.
+  -- Otherwise, scroll it to keep (cx,cy) as close to the center of the screen
+  -- as possible, without showing black space at the edges.
+  if self.w <= sw then
+    rw = self.w
+    ox = 0
+    dx = ((sw - self.w)/2):floor()
+  else
+    rw = sw
+    ox = math.bound(0, cx - sw/2, self.w - sw):floor()
+    dx = -ox
+  end
+
+  if self.h <= sh then
+    rh = self.h
+    oy = 0
+    dy = (sh - self.h)/2
+  else
+    rh = sh
+    oy = math.bound(0, cy - sh/2, self.h - sh):floor()
+    dy = -oy
+  end
+
+  game.log("draw: %d,%d+%d+%d (%dx%d)", ox, oy, rw, rh, sw, sh)
+  for x=ox,ox+rw-1 do
+    for y=oy,oy+rh-1 do
+      assert(x >= 0 and x < self.w, "x out of bounds: "..x)
+      assert(y >= 0 and y < self.h, "y out of bounds: "..y)
+
+      local cell = self[x+1][y+1]
+      if #cell > 0 then
+        tty.put(x + dx, y + dy, cell[#cell]:render())
       end
     end
   end
 end
 
 function map:placeAt(ent, object, x, y)
-  game.log("%s:placeAt(%s, %d, %d)", ent, object, x, y)
+  --game.log("%s:placeAt(%s, %d, %d)", ent, object, x, y)
   table.insert(self[x][y], object)
 end
 
 function map:removeFrom(ent, object, x, y)
-  game.log("%s:removeFrom(%s, %d, %d)", ent, object, x, y)
+  --game.log("%s:removeFrom(%s, %d, %d)", ent, object, x, y)
   local i,objs = 1,self[x][y]
   local removals = 0
   while i <= #objs do
--- ui.lua
+++ ui.lua
@@ -1,13 +1,19 @@
 ui = {}
 
+local last_init,last_hud,last_map,last_log,last_tty,last_frame = 0,0,0,0,0,0
 function ui.draw(player)
+  game.log("        Log: %.3f TTY: %.3f", last_log, last_tty)
+  game.log("        HUD: %.3f Map: %.3f", last_hud, last_map)
+  game.log("Render: Ini: %.3f All: %.3f", last_init, last_frame)
+
+  local t = os.clock()
+
   local w,h = tty.size()
   tty.clear()
   local x,y,map = player:position()
 
   -- Calculate viewport offset.
   -- x and y are the offset to add to coordinates before they go to the screen.
-  -- dx and dy are the upper left corner of the rectangle on the map to render.
   -- w and h are the dimensions of that rectangle.
   local hud_win = {
     x = 0; y = 0;
@@ -27,16 +33,17 @@ function ui.draw(player)
   tty.vline(map_win.x-1)
   tty.vline(log_win.x-1)
 
+  last_init = os.clock()
+
   tty.pushwin(hud_win)
   tty.colour(0, 0, 0, 255, 0, 0)
   tty.box(0, 0, hud_win.w, hud_win.h)
   tty.popwin()
 
+  last_hud = os.clock()
 
-  tty.pushwin(log_win)
-  tty.colour(0, 0, 0, 0, 0, 255)
-  tty.box(0, 0, log_win.w, log_win.h)
   tty.colour(255, 255, 255, 0, 0, 0)
+  tty.pushwin(log_win)
   local log = game.getLog()
   for i=1,h do
     local line = (log[#log-i+1] or ""):sub(1, (w/2):floor())
@@ -44,12 +51,21 @@ function ui.draw(player)
   end
   tty.popwin()
 
+  last_log = os.clock()
+
   tty.pushwin(map_win)
-  tty.colour(0, 0, 0, 0, 255, 0)
-  tty.box(0, 0, map_win.w, map_win.h)
-  tty.colour(255, 255, 255, 0, 0, 0)
-  --map:render_screen(x, y)
+  map:render_screen(x, y)
   tty.popwin()
 
+  last_map = os.clock()
+
   tty.flip()
+
+  last_tty = os.clock()
+  last_frame = last_tty - t
+  last_tty = last_tty - last_map
+  last_map = last_map - last_log
+  last_log = last_log - last_hud
+  last_hud = last_hud - last_init
+  last_init = last_init - t
 end
