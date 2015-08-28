c40e46da3450857dd7c46e97bc91438ed43446c9
Ben Kelly <bk@ancilla.ca>
2015-08-27 21:10:41 -0400

    fix off-by-one error in log renderer.

--- ui/log_win.lua
+++ ui/log_win.lua
@@ -7,9 +7,10 @@ function LogWin:render()
   ui.box(nil, 'Log')
   local h = self.h
   local log = game.getLog()
-  for y=h-2,1,-1 do
-    local line = (log[#log-h+y+1] or ""):sub(1, self.w-2)
-    tty.put(1, y, line)
+  for i=#log,1,-1 do
+    local y = self.h - 2 - #log + i
+    if y < 1 then break end
+    tty.put(1, y, log[i]:sub(1, self.w-2))
   end
 end
 
