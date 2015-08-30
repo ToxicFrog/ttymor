0e99163bf3a84518bba16934411214480bf3f0b7
Ben Kelly <bk@ancilla.ca>
2015-08-30 12:01:51 -0400

    Switch to a copy-based approach to subclassing & instantiation

--- Object.lua
+++ Object.lua
@@ -1,21 +1,33 @@
 Object = {}
+Object.__class = Object
 local ClassMT = {}
 
 function ClassMT:__call(...)
-	local obj = setmetatable({}, self)
-	obj:__init(...)
-	return obj
-end
-
-function Object:subclass(def)
-	table.merge(def, self, 'ignore')
-	def.__index = def
-	setmetatable(def, ClassMT)
-	return def
+  -- Copy all non-meta fields into the object
+  local obj = {}
+  for k,v in pairs(self) do
+    if type(k) ~= 'string' or not k:match('^__') then
+      obj[k] = v
+    end
+  end
+  setmetatable(obj, self)
+  obj:callmetamethod('__init', ...)
+  return obj
 end
 
 function Object:__init(kvs)
-	table.merge(self, kvs, 'overwrite')
+  table.merge(self, kvs, 'overwrite')
+end
+
+function Object:callmetamethod(name, ...)
+  return getmetafield(self, name)(self, ...)
+end
+
+function Object:subclass(def)
+  table.merge(def, self, 'ignore')
+  def.__class = def
+  setmetatable(def, ClassMT)
+  return def
 end
 
 setmetatable(Object, ClassMT)
