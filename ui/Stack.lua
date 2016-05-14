4f3b402b8235901749bc91ba260d482d29e74dd1
Ben Kelly <bk@ancilla.ca>
2016-05-14 17:59:35 -0400

    Render everything in the Stack, but still only pass events to the top

--- ui/Stack.lua
+++ ui/Stack.lua
@@ -1,18 +1,11 @@
 local Stack = ui.Window:subclass {}
 
-function Stack:attach(child)
-  if child and #self._children > 0 then
-    self._children[#self._children].visible = false
-  end
-  ui.Window.attach(self, child)
-end
-
-function Stack:detach(child)
-  ui.Window.detach(self, child)
-  if child and #self._children > 0 then
-    self._children[#self._children].visible = true
-    self:layout()
-  end
+function Stack:keyEvent(key, cmd)
+  if not self.visible then return false end
+  if self.can_focus and not self.focused then return false end
+  local top = self._children[#self._children]
+  if top and top:keyEvent(key, cmd) == true then return true end
+  return self:handleEvent(key, cmd)
 end
 
 return Stack
