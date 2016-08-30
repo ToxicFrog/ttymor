d59157836504a8bed2b286839f3e20750c1d831d
Ben Kelly <bk@ancilla.ca>
2016-08-29 22:10:37 -0400

    Add handling for <customblocker> and <customengraving> tags

--- dredmor/Room.lua
+++ dredmor/Room.lua
@@ -5,13 +5,20 @@ local Room = Object:subclass {
 
 local tags = {}
 
+-- The <room> tag, in theory, contains information like the dimensions of the
+-- room as well as the name. Unfortunately, this information is sometimes
+-- untrustworthy; we take only the name and infer the rest from the <row> tags.
+function tags:room(xml)
+  self.name = xml.attr.name
+end
+
 -- A <row> tag contains a single row of terrain; all of the <row> tags together
 -- make up the complete terrain of the level, along with any waypoints.
 function tags:row(row)
+  -- This will result in table indices {0..{w,h}-1}. This is intentional.
   local x = 0
-  local y = self.h+1
+  local y = self.h
   for char in row.attr.text:gmatch('.') do
-    x = x+1
     if char:match('%d') then
       self.at[char] = {x,y}
       char = '.'
@@ -21,6 +28,7 @@ function tags:row(row)
     end
     self[x] = self[x] or {}
     self[x][y] = char
+    x = x+1
   end
   self.w = self.w:max(#row.attr.text:trim())
   self.h = self.h + 1
@@ -37,11 +45,63 @@ function tags:flags(xml)
   end
 end
 
+function Room:locationFromAttr(attr)
+  local x,y,at = attr.x,attr.y,attr.at
+  if x and y and not at then
+    return assert(tonumber(x)),assert(tonumber(y))
+  elseif at and not y and not x then
+    return unpack((assertf(
+      self.at[at],
+      "rooms.xml: entity has at=%s but map grid doesn't.",
+      at)))
+  else
+    error("rooms.xml: entity has both x,y and at (or neither)")
+  end
+end
+
+function tags:customblocker(xml)
+  local png = xml.attr.png or xml.attr.pngSprite;
+  local x,y = self:location(xml.attr)
+  table.insert(self.entities, {
+    type = "Wall";
+    name = xml.attr.name;
+    desc = xml.attr.description;
+    x = x; y = y;
+    Render = { face = '♦'; png = png };
+  })
+end
+
+function tags:customengraving(xml)
+  local png = xml.attr.png or xml.attr.pngSprite;
+  local x,y = self:location(xml.attr)
+  table.insert(self.entities, {
+    type = "Floor";
+    name = xml.attr.name;
+    desc = xml.attr.description;
+    x = x; y = y;
+    Render = { face = '◊'; png = png };
+  })
+end
+
+--[[
+Tags we still need to handle:
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <action>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <condition>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <custombreakable>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <element>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <horde>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <loot>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <monster>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <pedestal>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <script>
+W @./dredmor/Room.lua:49] Unrecognized tag in rooms.xml: <trap>
+]]
+
 -- Unlike most ctors, this takes an XML <room> node as its argument rather than
 -- an initializer table.
 function Room:__init(root)
-  self.name = root.attr.name
   self.at = {}  -- Item positions read from terrain grid; to be filled in by grid reader
+  self.entities = {}
   for tag in xml.walk(root) do
     if tags[tag.name] then
       tags[tag.name](self, tag)
--- test/dredmor/rooms.lua
+++ test/dredmor/rooms.lua
@@ -21,4 +21,17 @@ function TestDredmorRoomsXML:testLoadRooms()
   lu.assertEquals(room.w, 5)
   lu.assertEquals(room.h, 5)
   lu.assertEquals(room.footprint, 20)
+  lu.assertTrue(room.special)
+  lu.assertEquals(room.minlevel, 2)
+  lu.assertEquals(room.maxlevel, 3)
+  lu.assertEquals(room.entities[1], {
+    type = "Wall"; name = "Test Object XY"; x = 3; y = 3;
+    desc = "using coordinates";
+    Render = { face = '♦' };
+  })
+  lu.assertEquals(room.entities[2], {
+    type = "Floor"; name = "Test Object At"; x = 1; y = 1;
+    desc = "using waypoint";
+    Render = { face = '◊' };
+  })
 end
