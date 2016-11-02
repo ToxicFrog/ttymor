c12bc010c424014248257c8c0e98970f80c7a167
Ben Kelly <bk@ancilla.ca>
2016-11-01 22:01:16 -0400

    Add skeleton of clojure project, Apache 2.0 license

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject ttymor "0.1.0-SNAPSHOT"
+  :description "A TTY-based 'demake' of Dungeons of Dredmor"
+  :url "https://github.com/toxicfrog/ttymor"
+  :license {:name "Apache 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.typed "0.3.26"]
+                 [com.googlecode.lanterna/lanterna "3.0.0-beta3"]]
+  :main ^:skip-aot ttymor.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/ttymor/core.clj
@@ -0,0 +1,7 @@
+(ns ttymor.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
