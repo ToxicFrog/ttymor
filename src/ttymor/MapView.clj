0bfc6c703ff6a766ce66cfb77ea034595d4fd519
Ben Kelly <bk@ancilla.ca>
2017-11-05 22:56:24 -0500

    I have a really terrible interactive FOV display now

--- src/ttymor/MapView.clj
+++ src/ttymor/MapView.clj
@@ -1,15 +1,40 @@
 (ns ttymor.MapView
   (:import (com.googlecode.lanterna TerminalSize TerminalPosition TextCharacter)
            (com.googlecode.lanterna.gui2 AbstractInteractableComponent InteractableRenderer Interactable$Result)
-           (com.googlecode.lanterna.graphics BasicTextImage)))
+           (com.googlecode.lanterna.graphics BasicTextImage)
+           [squidpony.squidgrid FOV Radius]))
+
+(defn map-grid [f grid]
+  (map (fn [slice] (map f slice))
+       grid))
+
+(defn terrain-to-vismap [terrain]
+  (->> terrain
+       (map-grid #(if (= \. %) 0.0 1.0))
+       (map double-array)
+       into-array))
+
+; returns an array<float>[w][h] of visibility information
+(defn fov-map [terrain x y r]
+  (let [fov (FOV.)]
+    (.calculateFOV fov
+      (terrain-to-vismap terrain) ; resistance map
+      x y r Radius/SQUARE)
+    ))
 
 (defn- image-from [terrain entities]
   (let [w (count terrain)
         h (count (first terrain))
-        image (BasicTextImage. w h)]
+        image (BasicTextImage. w h)
+        [px py] (get-in entities [0 :position])
+        fov (fov-map terrain px py 5)]
     (dorun
       (for [x (range w) y (range h)]
-        (.setCharacterAt image x y (TextCharacter. (get-in terrain [x y])))))
+        (.setCharacterAt image x y
+                         (if (> (aget fov x y) 0.0)
+                           (TextCharacter. (get-in terrain [x y]))
+                           (TextCharacter. \X))
+                         )))
     (dorun (map (fn [[id entity]]
                   (.setCharacterAt image
                                    (get-in entity [:position 0])
--- src/ttymor/core.clj
+++ src/ttymor/core.clj
@@ -19,46 +19,26 @@
   {0 {:position [1 1] :face \@}
    })
 
-(defn map-grid [f grid]
-  (map (fn [slice] (map f slice))
-       grid))
-
-(defn terrain-to-vismap [terrain]
-  (->> terrain
-       (map-grid #(if (= \. %) 0.0 1.0))
-       (map double-array)
-       into-array))
-       ; (to-array-2d)))
-
-; returns an array<float>[w][h] of visibility information
-(defn fov-map [terrain x y r]
-  (let [fov (FOV.)]
-    (.calculateFOV fov
-      (terrain-to-vismap terrain) ; resistance map
-      x y r Radius/SQUARE)
-    )
-  )
-
-(defn print-terrain [terrain px py r]
-  (let [fov (fov-map terrain px py r)
-        w (count terrain)
-        h (count (first terrain))]
-    (dorun (for [y (range h) x (range w)]
-             (do
-               (if (= 0 x) (print "\n"))
-               (cond
-                 (= [x y] [px py]) (print "@")
-                 (> (aget fov x y) 0.0) (print (get-in terrain [x y]))
-                 :else (print "/"))
-             ))))
-  (println ""))
+; (defn print-terrain [terrain px py r]
+;   (let [fov (fov-map terrain px py r)
+;         w (count terrain)
+;         h (count (first terrain))]
+;     (dorun (for [y (range h) x (range w)]
+;              (do
+;                (if (= 0 x) (print "\n"))
+;                (cond
+;                  (= [x y] [px py]) (print "@")
+;                  (> (aget fov x y) 0.0) (print (get-in terrain [x y]))
+;                  :else (print "/"))
+;              ))))
+;   (println ""))
 
 (defn -main [& args]
-  (println "main")
-  (print-terrain terrain 1 1 5)
-  (print-terrain terrain 1 2 5)
-  (print-terrain terrain 1 3 5)
-  (print-terrain terrain 2 3 5)
+  ; (println "main")
+  ; (print-terrain terrain 1 1 5)
+  ; (print-terrain terrain 1 2 5)
+  ; (print-terrain terrain 1 3 5)
+  ; (print-terrain terrain 2 3 5)
   (let [game (atom {:terrain terrain :entities entities})]
     (gui/run game))
   )
